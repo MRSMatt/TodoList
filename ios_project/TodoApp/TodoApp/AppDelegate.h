e1affc102a68b1840e0a86fd8a76d776aed97f20
Matt Linaberry <matt.linaberry@mrs-electronic.com>
2016-12-21 16:54:24 -0500

    lolz

---
+++ ios_project/TodoApp/TodoApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TodoApp
+//
+//  Created by Matthew Linaberry on 10/6/16.
+//  Copyright © 2016 Matthew Linaberry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios_project/TodoApp/TodoApp/main.mm
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TodoApp
+//
+//  Created by Matthew Linaberry on 10/6/16.
+//  Copyright © 2016 Matthew Linaberry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
